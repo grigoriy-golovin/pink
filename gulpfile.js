d7e3f358e0cdd4f68252349581b4e96d85080236
grigoriy-golovin <watson.180491@gmail.com>
2020-02-05 09:53:00 +0300

    настроенна сборка

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var less = require("gulp-less");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var webp = require("gulp-webp");
+var del = require("del");
+var server = require("browser-sync").create();
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("build"))
+});
+
+gulp.task("del", function () {
+  return del("build");
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(gulp.dest("build"))
+});
+
+gulp.task("css", function () {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/less/**/*.{scss,less}", gulp.series("css", "refresh"));
+  gulp.watch("source/img/*.{png,jpg}", gulp.series("webp", "refresh"));
+  gulp.watch("source/img/*.svg", gulp.series("refresh"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh")).on("change", server.reload);
+});
+
+gulp.task("build", gulp.series("del", "webp", "css", "html", "copy"));
+gulp.task("start", gulp.series("build", "server"));
